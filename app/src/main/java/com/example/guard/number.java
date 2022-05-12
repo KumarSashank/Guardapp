0701f4ad3bc4935e17dde4ed346b70b1e3e26598
KumarSashank <kumarsashank_ganta@srmap.edu.in>
2022-05-12 23:52:37 +0530

    added login intent to number activity

--- app/src/main/java/com/example/guard/number.java
+++ app/src/main/java/com/example/guard/number.java
@@ -7,14 +7,18 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class number extends AppCompatActivity {
-    TextView motp;
+    TextView motp,log;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_number);
         motp = (TextView) findViewById(R.id.otp);
+        log = (TextView) findViewById(R.id.num2log);
         motp.setOnClickListener(v -> {
             Toast.makeText(getApplicationContext(), "OTP sent successfully", Toast.LENGTH_LONG).show();
+        });
+        log.setOnClickListener(v -> {
+            startActivity(new android.content.Intent(getApplicationContext(), login.class));
         });
     }
 }
